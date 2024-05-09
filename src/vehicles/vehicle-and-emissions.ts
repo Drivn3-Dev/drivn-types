4ae6364f06626b3ad5b8a39f5e2d4dcb5bea760a
SimonTheMachine <simon@alsbirk.com>
2024-05-09 11:24:24 +0200

    updated types

--- src/vehicles/vehicle-and-emissions.ts
+++ src/vehicles/vehicle-and-emissions.ts
@@ -13,25 +13,21 @@ const emissionsData: Record<string, Record<string, Record<string, number>>> = {
       "small": 0.140798534,
       "medium": 0.178188534,
       "large": 0.272238534,
-      "average": 0.163908534,
     },
     "diesel": {
       "small": 0.139306449,
       "medium": 0.167156449,
       "large": 0.208586449,
-      "average": 0.169826449,
     },
     "hybrid": {
       "small": 0.101498858,
       "medium": 0.109038436,
       "large": 0.1524358,
-      "average": 0.118978588,
     },
     "electric": {
       "small": 0.044399301,
       "medium": 0.048457271,
       "large": 0.053355334,
-      "average": 0.050427364,
     },
   },
   "motorbike": {
@@ -39,7 +35,6 @@ const emissionsData: Record<string, Record<string, Record<string, number>>> = {
       "small": 0.08319,
       "medium": 0.10108,
       "large": 0.13252,
-      "average": 0.113675,
     },
   },
 };
