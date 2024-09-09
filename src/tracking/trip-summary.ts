06a6f594fe268da23babd4ece9b4599ce6ab8c35
Tomasrieck <Tomasrieck@gmail.com>
2024-09-09 12:30:01 +0200

    updated types

--- src/tracking/trip-summary.ts
+++ src/tracking/trip-summary.ts
@@ -17,7 +17,7 @@ export interface TripSummary {
   /** Boolean stating if the trip has been confirmed */
   correct: boolean;
 
-  /* Whether the trip is to the office or not. */
+  /** Whether the trip is to the office or not. */
   toOffice: boolean;
 
   /** Boolean stating if the trip was private */
