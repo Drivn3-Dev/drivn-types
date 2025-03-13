577637d05552218efd478c74ddf48d3747de7170
Tomasrieck <Tomasrieck@gmail.com>
2025-03-13 13:35:28 +0100

    updated types

--- src/tracking/trip-times.ts
+++ src/tracking/trip-times.ts
@@ -18,7 +18,7 @@ export interface TripInfoShort {
   toOffice: boolean;
 
   /** Array of distinct activity types involved in the trip */
-  activityTypes?: string[];
+  activityTypes?: Record<Drivn.ActivityType, number>;
 
   /** Boolean stating if the trip was private */
   private?: boolean;
