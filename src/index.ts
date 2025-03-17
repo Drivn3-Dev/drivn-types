fc641cc211e8ce28ad94575e18d0cce54b1d2874
Tomasrieck <Tomasrieck@gmail.com>
2025-03-17 16:31:40 +0100

    updated types

--- src/index.ts
+++ src/index.ts
@@ -198,6 +198,8 @@ declare global {
     type DepartmentUpdate = Omit<Department, "id" | "employees">;
     type OfficeCreate = Omit<Office, "id" | "employees" | "orgId">;
     type OfficeUpdate = Omit<Office, "id" | "orgId" | "employees">;
+    type ResidenceCreate = Omit<Residence, "id">;
+    type ResidenceUpdate = Omit<Residence, "id">;
     type ReportCreate = Omit<Report, "tripId" | "userId" | "id" | "date">;
     type UserCreate = Omit<UserWithCreds, "timeAdded" | "uid">;
     type UserUpdate = Partial<Omit<User, "uid" | "timeAdded">>;
