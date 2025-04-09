7a3296ed6d74198a603a81c9a14f93892e8e7843
Tomasrieck <Tomasrieck@gmail.com>
2025-04-09 21:17:59 +0200

    updated types

--- src/user/user.ts
+++ src/user/user.ts
@@ -75,13 +75,13 @@ export interface User {
 }
 
 export interface UserWorkDays {
-  "Monday": {active: boolean, hours: string[]};
-  "Tuesday": {active: boolean, hours: string[]};
-  "Wednesday": {active: boolean, hours: string[]};
-  "Thursday": {active: boolean, hours: string[]};
-  "Friday": {active: boolean, hours: string[]};
-  "Saturday": {active: boolean, hours: string[]};
-  "Sunday": {active: boolean, hours: string[]};
+  Monday: {active: boolean, hours: string[] | undefined};
+  Tuesday: {active: boolean, hours: string[] | undefined};
+  Wednesday: {active: boolean, hours: string[] | undefined};
+  Thursday: {active: boolean, hours: string[] | undefined};
+  Friday: {active: boolean, hours: string[] | undefined};
+  Saturday: {active: boolean, hours: string[] | undefined};
+  Sunday: {active: boolean, hours: string[] | undefined};
 }
 
 export interface UserForInvite extends Drivn.UserForCreate {
