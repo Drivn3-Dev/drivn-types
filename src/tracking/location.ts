52ea5ba524b84254d13a36d6c53e709a3bccca18
Tomasrieck <Tomasrieck@gmail.com>
2025-05-05 17:58:07 +0200

    updated types

--- src/tracking/location.ts
+++ src/tracking/location.ts
@@ -45,7 +45,7 @@ export type CurrentDrive = LocationTimestamp[];
 
 export interface LogRequestBody {
   // The Current Date Time
-  locations: Drivn.CurrentDrive;
+  locations: Drivn.NewTrip;
 
   // Optional Trip Id
   tripId?: string;
