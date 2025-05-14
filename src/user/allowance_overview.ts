f35dc9da2ed3c3aaece4310da18f967a6ece9bb5
Tomasrieck <Tomasrieck@gmail.com>
2025-05-14 11:18:05 +0200

    updated types

---
+++ src/user/allowance_overview.ts
@@ -0,0 +1,40 @@
+export interface AllowanceOverview {
+  // The Overview Unique Id
+  id: string;
+
+  // The User Id for the user who created the report
+  userId: string;
+
+  // The Date When The Overview was Created
+  createdAt: Date;
+
+  // The Start Date for the Overview
+  startDate: Date;
+
+  // The End Date for the Overview
+  endDate: Date;
+
+  // The Trip Ids in the overview
+  tripIds: string[];
+
+  // The Distance Driven in Business Context in km
+  businessMilage: number;
+
+  // The Distance Driven in Private Context in km
+  privateMilage: number;
+
+  // The Commute Distance in km
+  commuteMilage: number;
+
+  // The Emissions in Business Context in kg CO2
+  businessEmissions: number;
+
+  // The Emissions in Private Context in kg CO2
+  privateEmissions: number;
+
+  // The Commute Emissions in kg CO2
+  commuteEmissions: number;
+
+  // The amount of reimbursement the user is entitled to
+  reimbursement: number;
+}
