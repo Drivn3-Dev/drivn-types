58b7aff89e418ac7a755792d5c332591d054be02
Tomasrieck <Tomasrieck@gmail.com>
2025-05-16 15:35:34 +0200

    updated types

--- src/index.ts
+++ src/index.ts
@@ -207,7 +207,7 @@ declare global {
     type ResidenceCreate = Omit<Residence, "id" | "uid">;
     type ResidenceUpdate = Omit<Residence, "uid">;
     type ReportCreate = Omit<Report, "tripId" | "userId" | "id" | "date">;
-    type AllowanceOverviewCreate = Omit<AllowanceOverviewWithTripInfos, "id" | "userId">;
+    type AllowanceOverviewCreate = Omit<AllowanceOverviewWithTripInfos, "id" | "userId" | "tripIds">;
     type UserCreate = Omit<UserWithCreds, "timeAdded" | "uid">;
     type UserUpdate = Partial<Omit<User, "uid" | "timeAdded">>;
     type ToggleAnonymous = {
